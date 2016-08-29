667ff44e40ff3a2db44fb355a28ce57e352c4939
ivanhanigan <ivan.hanigan@gmail.com>
2016-08-29 16:40:26 +1000

    add code to do calc

--- code/func.R
+++ code/func.R
@@ -2,3 +2,8 @@
 'name:func'
 library(rgdal)
 library(raster)
+if(!require(HutchinsonDroughtIndex)){
+library(devtools)
+install_github("ivanhanigan/HutchinsonDroughtIndex")
+library(HutchinsonDroughtIndex)
+}
