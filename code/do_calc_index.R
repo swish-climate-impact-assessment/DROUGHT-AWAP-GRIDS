838742681e81eb8ac574e958d4cd0073dac3492a
ivanhanigan <ivan.hanigan@gmail.com>
2016-09-02 11:24:23 +1000

    add loop for varying 6 or 12 rolling sums

--- code/do_calc_index.R
+++ code/do_calc_index.R
@@ -8,43 +8,46 @@ indir <- "data_derived"
 
 states  <- c("act", "nsw", "nt",  "qld", "sa",  "tas", "vic", "wa")
 
-for(ste in states){
+for(ste in states[2]){
 #  ste  <- "act"
 infile <- paste("rain_",ste,"_1900_2015.csv", sep = "")
-outfile_main  <- paste("rain_",ste,"_1900_2015_drought.csv", sep = "")
-dat <- read.csv(file.path(indir, infile), as.is = T)
-#str(dat)
 
-dat$date  <- as.Date(paste(dat$year, dat$month, 1, sep = "-"))
-dat <- dat[,c("gid", "date","year","month","rain")]
-#tail(dat)
-#head(dat)
-
-  ##############################################
-  # do the drought algorithm
-  gids <- names(table(dat$gid))
-  #gids
-  for(i in 1:length(gids)){
-  #  gid_i  <- gids[1]
-    gid_i <- gids[i]
-    dat_i <- dat[dat$gid == gid_i,c("date", "year", "month", "rain")]
-  #str(dat_i)
-  drt <- drought_index_stations(
-    data=dat_i
-    ,
-    years=length(names(table(dat$year)))
-    ,
-    M = 6
-    ,
-    droughtThreshold = 0.375
-    )
-  #head(drt)
-  drt$gid  <- gid_i
-  write.table(drt,
-            file.path("data_derived", outfile_main)
-            , sep = ",", append = i != 1,
-            col.names = i == 1, row.names = F)
+for(m_i in c(6, 12)){
+  outfile_main  <- paste("rain_",ste,"_1900_2015_drought",m_i,".csv", sep = "")
+  dat <- read.csv(file.path(indir, infile), as.is = T)
+  #str(dat)
+   
+  dat$date  <- as.Date(paste(dat$year, dat$month, 1, sep = "-"))
+  dat <- dat[,c("gid", "date","year","month","rain")]
+  #tail(dat)
+  #head(dat)
+   
+    ##############################################
+    # do the drought algorithm
+    gids <- names(table(dat$gid))
+    #gids
+    for(i in 1:length(gids)){
+    #  gid_i  <- gids[1]
+      gid_i <- gids[i]
+      dat_i <- dat[dat$gid == gid_i,c("date", "year", "month", "rain")]
+    #str(dat_i)
+    drt <- drought_index_stations(
+      data=dat_i
+      ,
+      years=length(names(table(dat$year)))
+      ,
+      M = m_i
+      ,
+      droughtThreshold = 0.375
+      )
+    #head(drt)
+    drt$gid  <- gid_i
+    write.table(drt,
+              file.path("data_derived", outfile_main)
+              , sep = ",", append = i != 1,
+              col.names = i == 1, row.names = F)
+     
+    }
    
   }
-
 }
