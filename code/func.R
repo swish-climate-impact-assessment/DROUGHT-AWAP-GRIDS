62684193f26f3cc44a825bbce745c46214896d74
ivanhanigan <ivan.hanigan@gmail.com>
2016-09-08 01:11:52 +0000

    rm code files on rstudio server

--- code/func.R
+++
@@ -1,9 +0,0 @@
-
-'name:func'
-library(rgdal)
-library(raster)
-if(!require(HutchinsonDroughtIndex)){
-library(devtools)
-install_github("ivanhanigan/HutchinsonDroughtIndex")
-library(HutchinsonDroughtIndex)
-}
