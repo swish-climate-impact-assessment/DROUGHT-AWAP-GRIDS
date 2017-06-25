1841b64bca227636d9c89f0ae2b19ec7f738fde1
Ivan <ivan_hanigan@swish3.novalocal>
2017-06-25 01:12:49 +0000

    notes re convert2shp and linux mv

--- code/do_convert2shp.R
+++ code/do_convert2shp.R
@@ -3,27 +3,33 @@
 projdir <- "~/projects/DROUGHT-AWAP-GRIDS/DROUGHT_AWAP_GRIDS_1900_2015"
 setwd(projdir)
 source("code/func.R")
-states  <- c("act",  "nt",  "qld", "sa",  "tas", "vic", "wa") # "nsw",
-for(ste in states){
- # ste  <- "nsw"
 
-  indir_shp <- "~/projects/DROUGHT-BOM-GRIDS/data_derived"
+indir_data <- "/home/ResearchData/DROUGHT-AWAP-GRIDS/DROUGHT_AWAP_GRIDS_1900_2015/data_derived"
+dir.create("data_derived_shapefiles")
+
+
+states  <- c("act",  "nt",  "qld", "sa",  "tas", "vic", "wa") # "nsw",
+# do one at a time so can do 7zip before mv and chmod
+for(ste in states[1]){
+  #ste  <- "vic"
+
+  indir_shp <- "~/ResearchData/DROUGHT-BOM-GRIDS/data_derived"
   #gsub("\\.shp","",gsub("grid_", "", dir(indir_shp, pattern = ".shp")))
   setwd(indir_shp)
   shp  <- readOGR(".", sprintf("grid_%s", ste))
   setwd(projdir)
   #plot(shp)
-for(m_i in c(6, 12)){
-  #m_i  <- 6
+#for(m_i in c(6, 12)){
+  m_i  <- 6
   infile <- paste("rain_",ste,"_1900_2015_drought",m_i,".csv", sep = "")
-  indat <- read.csv(file.path("data_derived", infile), as.is = T)
+  indat <- read.csv(file.path(indir_data, infile), as.is = T)
   #str(indat)
   head(indat)
   # do loop
-  for(y in c(1986:2001,2004:2012)){
+  for(y in c(1900:2015)){
     #y  <- 2015
     for(m in 1:12){
-    # m  <- 12
+    # m  <- 1
     m2 <- sprintf("%02d", m)
   outfile_main  <- paste("drought_awap_grids_", ste, "_",y,"_",m2,"_roll",m_i, sep  = "")
 
@@ -37,5 +43,22 @@ for(m_i in c(6, 12)){
    , driver = "ESRI Shapefile", overwrite_layer=T)
     }
   }
+#}
 }
-}
+dir("data_derived_shapefiles")
+source("~/tools/disentangle/R/checkout7z.R")
+cmd  <- checkout7z(dirlist = "data_derived_shapefiles/"
+                   ,
+                   archive_id = "drought_awap_grids_1900_2015_act_roll6",
+                   loc_7z = "7z")
+cat(cmd)
+dir()
+system(cmd)
+
+# and then from shell, as sudo
+"
+mv /home/ivan_hanigan/projects/DROUGHT-AWAP-GRIDS/DROUGHT_AWAP_GRIDS_1900_2015/data_derived_shapefiles/* /home/ResearchData/DROUGHT-AWAP-GRIDS/DROUGHT_AWAP_GRIDS_1900_2015/data_derived_shapefiles/
+
+chown -R root:researchdata /home/ResearchData/DROUGHT-AWAP-GRIDS/DROUGHT_AWAP_GRIDS_1900_2015/data_derived_shapefiles/
+
+"
