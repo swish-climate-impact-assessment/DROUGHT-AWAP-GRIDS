a3ad545b8fc01fcd8f12e4552b383432a04ed7e4
ivanhanigan <ivan.hanigan@gmail.com>
2017-06-25 11:20:19 +1000

    add local conv2shp

--- main.R
+++ main.R
@@ -14,7 +14,7 @@ source("code/func.R")
 
 'name:extract from awapgrids'
 
-indir  <- "~/ResearchData/AWAP_GRIDS/AWAP_GRIDS_1900_2015/data"
+indir  <- "~/ResearchData/AWAP_GRIDS/data"
 dir(indir)
 infilelist <- dir(indir, pattern = ".tif$", full.names=T)
 infilelist <- infilelist[grep("total", infilelist)]
@@ -28,22 +28,22 @@ for(ste in states[1:4]){
   setwd(indir_shp)
   shp  <- readOGR(".", sprintf("grid_%s", ste))
   setwd(projdir)
-
-
+  
+  
   #str(shp)
   shp2 <- rgeos::gCentroid(shp, byid = T)
 #  plot(shp)
 #  plot(shp2, add = T, pch = 16)
 
 
-
+    
 for(i in 1:length(infilelist)){
   #i = 1
   infile <- infilelist[i]
   outfile <- gsub("GTif_", "", gsub(".tif", "", basename(infile)))
   y  <- substr(outfile, 8, 8 +3)
   m  <- substr(outfile, 8 +4, 8+5)
-
+  
   #print(infile)
   r  <- raster(infile)
   shpout <- shp
@@ -63,7 +63,7 @@ for(i in 1:length(infilelist)){
   csvout$the_geom  <- NULL
   csvout$the_geom_p  <- NULL
   csvout$wronglatit  <- NULL
-  csvout$admin_name  <- NULL
+  csvout$admin_name  <- NULL  
   names(csvout) <- gsub("^e$", "rain", names(csvout))
   csvout$year <- as.numeric(y)
   csvout$month <- as.numeric(m)
